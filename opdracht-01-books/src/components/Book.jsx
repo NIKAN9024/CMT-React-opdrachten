bcc54c2d8c26e92af9b547bcb8abfc94d6607579
NIKAN9024 <9024387@student.tcrmbo.nl>
2025-10-15 10:45:25 +0200

    books uitbreiding

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,9 +1,18 @@
+import { useState} from 'react';
+
 const Book = ({img, title, author}) => {
+  const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
+  const verHoogTeller = () => {
+    setAantalKeerGelezen(aantalKeerGelezen + 1);
+  }
   return (
     <section className="book">
       <img src={img} alt="{title}" />
       <h2>{title}</h2>
       <h3>{author}</h3>
+      <button onClick={verhoogTeller}>
+        Keer gelezen: {2}
+      </button>
     </section>
   );
 };
